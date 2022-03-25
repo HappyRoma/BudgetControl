05f96df7f8b62fa1772d54c7a3f2fb160ae3580e
HappyRoma <krivonogov.roma@mail.ru>
2022-03-25 20:17:29 +0500

    Добавлены 2 компоненты. Созданы роуты между ними

--- budget/src/app/app.module.ts
+++ budget/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
