03e2550e909624eb260619c2decadcdca0fb014e
HappyRoma <krivonogov.roma@mail.ru>
2022-05-07 00:02:18 +0500

    colored-currency.component.ts теперь в качестве Input'а может принимать тип Observable<number>.

--- budget/src/app/children/cabinet/components/colored-currency/colored-currency.component.ts
+++ budget/src/app/children/cabinet/components/colored-currency/colored-currency.component.ts
@@ -1,15 +1,17 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'colored-currency',
   template: `
-    <span [ngStyle]="{color: negativeValue(value) ? 'red' : 'green'}"> {{res | customCurrency}} </span>
+    <span [ngStyle]="{color: negativeValue(val) ? 'red' : 'green'}"> {{res | customCurrency}} </span>
   `,
 })
 export class ColoredCurrencyComponent implements OnInit {
 
-  @Input() value!: number;
+  @Input() value!: number | Observable<number>;
 
+  val: number = 0;
   res: number = 0;
 
   public negativeValue(a: number) {
@@ -20,8 +22,15 @@ export class ColoredCurrencyComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.value) {
+    if (typeof this.value === 'number') {
+      this.val = this.value;
       this.res = this.value < 0 ? +this.value.toString().slice(1) : this.value;
+    }
+    else {
+      this.value.subscribe(value1 => {
+        this.val = value1;
+        this.res = value1 < 0 ? +value1.toString().slice(1) : value1;
+      })
     }
   }
 
